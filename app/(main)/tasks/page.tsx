3cf3683bb8bd1942507fe74ca9919e96d92fe0ca
tiesen243 <ttien56906@gmail.com>
2024-07-14 17:19:41 +0700

    feat(tasks): add task filtering by status

--- app/(main)/tasks/page.tsx
+++ app/(main)/tasks/page.tsx
@@ -1,15 +1,57 @@
 import type { NextPage } from 'next'
 
 import { TaskList } from '@/components/task/list'
+import { api, HydrateClient } from '@/lib/trpc/server'
+import Link from 'next/link'
 
-const Page: NextPage = async () => {
+interface Props {
+  searchParams: {
+    isDone?: string
+  }
+}
+
+const Page: NextPage<Props> = async ({ searchParams }) => {
+  void api.task.getTasks.prefetch({})
+  const isDone = searchParams.isDone ? searchParams.isDone.toLowerCase() === 'true' : undefined
+
   return (
-    <>
+    <HydrateClient>
       <h1 className="my-4 text-2xl font-bold">Tasks</h1>
 
-      <TaskList />
-    </>
+      <ul className="mb-4 flex gap-2">
+        {filterTasks.map((filter, idx) => (
+          <li key={idx}>
+            <Link
+              href={filter.href}
+              className={`rounded-md px-2 py-1 ${isDone === filter.value ? 'bg-accent text-accent-foreground' : ''}`}
+            >
+              {filter.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <TaskList isDone={isDone} />
+    </HydrateClient>
   )
 }
 
 export default Page
+
+const filterTasks = [
+  {
+    label: 'All',
+    value: undefined,
+    href: '/tasks',
+  },
+  {
+    label: 'Done',
+    value: true,
+    href: '/tasks?isDone=true',
+  },
+  {
+    label: 'Not Done',
+    value: false,
+    href: '/tasks?isDone=false',
+  },
+]
--- components/task/list.tsx
+++ components/task/list.tsx
@@ -3,8 +3,8 @@
 import { api } from '@/lib/trpc/react'
 import { TaskCard } from './card'
 
-export const TaskList: React.FC = () => {
-  const [tasks] = api.task.getTasks.useSuspenseQuery({})
+export const TaskList: React.FC<{ isDone?: boolean }> = ({ isDone }) => {
+  const [tasks] = api.task.getTasks.useSuspenseQuery({ isDone })
 
   return (
     <section className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
