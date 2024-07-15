a911844b98c265921d40b92b90263e8b42cc10f0
tiesen243 <ttien56906@gmail.com>
2024-07-15 17:20:56 +0700

    feat(page.tsx): remove prefetching of task deadlines
    The prefetching of task deadlines in the main page has been removed.
    This change simplifies the code and potentially improves performance
    by reducing unnecessary network requests.

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,18 +1,14 @@
 import type { NextPage } from 'next'
 
-import { api, HydrateClient } from '@/lib/trpc/server'
 import { DeadlineList } from '@/components/task/deadlines'
+import { HydrateClient } from '@/lib/trpc/server'
 
-const Page: NextPage = () => {
-  void api.task.getDeadlines.prefetch()
+const Page: NextPage = () => (
+  <HydrateClient>
+    <h1 className="my-4 text-2xl font-bold">Deadlines</h1>
 
-  return (
-    <HydrateClient>
-      <h1 className="my-4 text-2xl font-bold">Deadlines</h1>
-
-      <DeadlineList />
-    </HydrateClient>
-  )
-}
+    <DeadlineList />
+  </HydrateClient>
+)
 
 export default Page
