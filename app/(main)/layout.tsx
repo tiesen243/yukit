b7516a4d1dd093b863a386572ae71f7aa3db86f7
tiesen243 <ttien56906@gmail.com>
2024-07-21 03:34:28 +0700

    feat(layout): add authentication check to MainLayout
    The MainLayout component now checks if a user is authenticated
    using the Clerk library. If the user is not authenticated, they
    are redirected to the landing page.

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,17 @@
 import { Header } from '@/components/header'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 
-const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <>
-    <Header />
-    <main>{children}</main>
-  </>
-)
+const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const { userId } = auth()
+  if (!userId) redirect('/landing')
+
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  )
+}
 
 export default MainLayout
